2e9eee9ca27c48e3189d902a84218a0137ba3092
Oliver Mazorodze <olivermazorodze1@gmail.com>
2026-03-25 11:33:34 +0200

    initial

---
+++ backend/expertDB/src/main/java/org/innov/expertdb/ExpertDbApplication.java
@@ -0,0 +1,15 @@
+package org.innov.expertdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpertDbApplication.class, args);
+
+      //test  
+    }
+
+}
---
+++ backend/expertDB/src/test/java/org/innov/expertdb/ExpertDbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.innov.expertdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
