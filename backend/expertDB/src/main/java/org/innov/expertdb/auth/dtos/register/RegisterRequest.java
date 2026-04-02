c5f5b239d0ce8cec79a23d14ceb62fabd0d16fdb
Oliver Mazorodze <olivermazorodze1@gmail.com>
2026-04-02 19:39:02 +0200

    Fixed

--- backend/expertDB/src/main/java/org/innov/expertdb/auth/dtos/register/RegisterRequest.java
+++
@@ -1,6 +0,0 @@
-package org.innov.expertdb.auth.dtos.register;
-
-import org.innov.expertdb.user.Role;
-
-public record RegisterRequest(
-        String name, String email, String password, Role role) { }
